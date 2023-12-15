115eddeab1a29eb2a15c6c436f174635406b392b
Tobias Stikvoort <tobias@q42.nl>
2023-12-15 13:31:03 +0100

    add fetch options for next client

--- src/next.ts
+++ src/next.ts
@@ -1,30 +1,41 @@
 import { PageTreeConfig, PageMetadata } from './types';
 import { getAllPageMetadata } from './helpers/page-tree';
 import { getRawPageMetadataQuery } from './queries';
-import { SanityClient } from 'next-sanity';
+import { FilteredResponseQueryOptions, SanityClient } from 'next-sanity';
 
 export type { PageMetadata } from './types';
 
 export type NextPageTreeClientOptions = {
   config: PageTreeConfig;
   client: SanityClient;
+  fetchOptions?: FilteredResponseQueryOptions;
 };
 
-export const createNextPageTreeClient = ({ config, client }: NextPageTreeClientOptions) => {
-  return new NextPageTreeClient(config, client);
+export const createNextPageTreeClient = ({ config, client, fetchOptions }: NextPageTreeClientOptions) => {
+  return new NextPageTreeClient(config, client, fetchOptions);
 };
 
 class NextPageTreeClient {
   private readonly config: PageTreeConfig;
   private readonly client: SanityClient;
+  private readonly defaultSanityFetchOptions: FilteredResponseQueryOptions;
 
-  constructor(config: PageTreeConfig, client: SanityClient) {
+  constructor(
+    config: PageTreeConfig,
+    client: SanityClient,
+    defaultSanityFetchOptions: FilteredResponseQueryOptions = {},
+  ) {
     this.config = config;
     this.client = client;
+    this.defaultSanityFetchOptions = defaultSanityFetchOptions;
   }
 
   public async getAllPageMetadata(): Promise<PageMetadata[]> {
-    const rawPageMetadata = await this.client.fetch(getRawPageMetadataQuery(this.config));
+    const rawPageMetadata = await this.client.fetch(
+      getRawPageMetadataQuery(this.config),
+      null,
+      this.defaultSanityFetchOptions,
+    );
     return getAllPageMetadata(this.config, rawPageMetadata);
   }
 
