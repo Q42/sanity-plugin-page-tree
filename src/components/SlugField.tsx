2a51331c24b50cf9906ffd0d7e6919c7afa0f99f
Tobias Stikvoort <tobias@q42.nl>
2024-02-06 13:50:15 +0100

    improve typing

--- src/components/SlugField.tsx
+++ src/components/SlugField.tsx
@@ -1,5 +1,5 @@
 import { Stack, Text } from '@sanity/ui';
-import { Reference, SlugInput, SlugInputProps, useEditState, useFormValue } from 'sanity';
+import { Reference, SlugInput, SlugInputProps, SlugValue, useEditState, useFormValue } from 'sanity';
 import { PageTreeConfig } from '../types';
 import { usePageTreeItem } from '../hooks/usePageTreeItem';
 import { getSanityDocumentId } from '../utils/sanity';
@@ -9,34 +9,43 @@ export type SlugFieldProps = {
 } & SlugInputProps;
 
 export const SlugField = (props: SlugFieldProps) => {
-  const id = useFormValue(['_id']) as string;
-  const type = useFormValue(['type']) as string;
-  const parentId = useFormValue(['parent']) as Reference;
-
-  const state = useEditState(getSanityDocumentId(id), type);
-  const isPublised = !!state.published;
+  const id = useFormValue(['_id']);
+  const type = useFormValue(['_type']);
+  // eslint-disable-next-line no-warning-comments
+  // TODO ideally this would be more type safe.
+  const parentRef = useFormValue(['parent']) as Reference | undefined;
 
   const { config, value, ...otherProps } = props;
-  // we use published perspective so we don't get a draft version of the slug that has been changed of a parent page.
-  const { page, isLoading } = usePageTreeItem(parentId!._ref, config, 'published');
-  const path = page?.path == '/' ? `${page?.path}${value?.current}` : `${page?.path}/${value?.current}`;
 
   return (
     <Stack space={3}>
       <SlugInput {...otherProps} value={value} />
-      {!isLoading && <UrlExplanation baseUrl={config.baseUrl} path={path} isPublised={isPublised} />}
+      {typeof id == 'string' && typeof type == 'string' && !!parentRef?._ref && (
+        <UrlExplanation id={id} type={type} parentId={parentRef?._ref} config={config} value={value} />
+      )}
     </Stack>
   );
 };
 
 type UrlExplanationProps = {
-  baseUrl?: string;
-  path: string;
-  isPublised: boolean;
+  id: string;
+  type: string;
+  parentId: string;
+  value: SlugValue | undefined;
+  config: PageTreeConfig;
 };
 
-const UrlExplanation = ({ baseUrl, path, isPublised }: UrlExplanationProps) => {
-  const url = baseUrl ? `${baseUrl}${path}` : null;
+const UrlExplanation = ({ id, type, parentId, value, config }: UrlExplanationProps) => {
+  const state = useEditState(getSanityDocumentId(id), type ?? '');
+  const isPublised = !!state.published;
+
+  // we use published perspective so we don't get a draft version of the slug that has been changed of a parent page.
+  const { page, isLoading } = usePageTreeItem(parentId, config, 'published');
+  if (isLoading) return null;
+
+  const path = page?.path == '/' ? `${page?.path}${value?.current}` : `${page?.path}/${value?.current}`;
+
+  const url = config.baseUrl ? `${config.baseUrl}${path}` : null;
   const linkToPage = url && (
     <a href={url} target="blank">
       {url}
