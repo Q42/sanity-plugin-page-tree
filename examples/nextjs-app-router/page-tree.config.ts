8c8059c1771c6642a061ab22de5f6d40c6ed3a88
Tobias Stikvoort <tobias@q42.nl>
2025-03-11 14:53:47 +0100

    cleanup

--- examples/nextjs-app-router/page-tree.config.ts
+++ examples/nextjs-app-router/page-tree.config.ts
@@ -1,9 +1,7 @@
 import { PageTreeConfig } from '@q42/sanity-plugin-page-tree';
-import { apiVersion } from './sanity/env';
 
 export const pageTreeConfig: PageTreeConfig = {
   rootSchemaType: 'homePage',
   pageSchemaTypes: ['homePage', 'contentPage'],
-  apiVersion,
   titleFieldName: 'title',
 };
--- examples/nextjs-app-router/sanity/env.ts
+++ examples/nextjs-app-router/sanity/env.ts
@@ -1,4 +1,5 @@
-export const apiVersion = '2023-12-15'
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-12-15'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
